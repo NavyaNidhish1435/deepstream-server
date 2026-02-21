bcaea1e4bc83e2533d775537a7e5b34351aec1a8
Navya Nidhish <145640655+NavyaNidhish1435@users.noreply.github.com>
2026-02-21 11:10:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-echo "===== DeepStream Auto Deployment ====="
-
-sudo apt update
-
-# Install docker if missing
-if ! command -v docker &> /dev/null
-then
-    curl -fsSL https://get.docker.com | sh
-    sudo usermod -aG docker $USER
-fi
-
-# Install NVIDIA toolkit
-if ! dpkg -l | grep -q nvidia-container-toolkit; then
-    sudo apt install -y nvidia-container-toolkit
-    sudo systemctl restart docker
-fi
+echo "Starting DeepStream container..."
 
 docker compose build
 docker compose up -d
 
-echo "DeepStream Started"
+echo "DeepStream running safely."
